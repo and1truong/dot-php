658ede83dcbbcb47df1bbd2bf9688b08fb0440d4
Andy Truong <thehongtt@gmail.com>
2019-03-20 11:57:05 +1000

    Update start script

--- start.bash
+++ start.bash
@@ -6,10 +6,12 @@ _IP=`ifconfig $_NETWORK | grep 'inet ' | awk '{print $2}' | grep -Eo '([0-9]*\\.
 echo "Host IP address: $_IP"
 
 docker run --rm -it \
-    --entrypoint sh \
     -v $HOME/.php/:/atm \
     -v $HOME/.php/container.ini:/usr/local/etc/php/conf.d/atm.ini \
+    -v $HOME/.php/bin/composer:/usr/local/bin/composer \
+    -v $HOME/.php/bin/phpunit:/usr/local/bin/phpunit \
     -v "$(pwd)":/app \
     -e XDEBUG_CONFIG="remote_host=$_IP" \
     -w /app \
+    --entrypoint sh \
     php:7.3-alpine
